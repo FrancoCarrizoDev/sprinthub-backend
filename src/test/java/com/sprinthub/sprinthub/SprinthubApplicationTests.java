506695ada80eacb9cd9ecb609c278abdc6bb2fac
FrancoCarrizoDev <francoadrianc@gmail.com>
2024-12-11 23:08:51 -0300

    init project

---
+++ src/test/java/com/sprinthub/sprinthub/SprinthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sprinthub.sprinthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
